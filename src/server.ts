7da36b8bb8bac4dbfda45cba7f206da0f733335d
anderson-alves427 <oliveiraalves427@gmail.com>
2022-10-12 20:06:31 -0300

    Fazendo conexao com banco de dados pg

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { clientesRoutes } from "./routes/clientes.routes";
 import { pessoasRoutes } from "./routes/pessoas.routes";
 
+import "./database";
+
 const app = express();
 app.use(express.json());
 
